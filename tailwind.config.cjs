6630b5ecb5fe37463161bfb9a9230b99d3d556e0
earlgray <earlgray283@gmail.com>
2023-02-26 00:14:12 +0900

    feat: add custom colors on tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'dark-iceblue': '#5382a1',
+        iceblue: '#99b7dc',
+        white: '#ffffffd0',
+      },
+    },
+  },
+  plugins: [],
+};
