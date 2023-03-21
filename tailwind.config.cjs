7161aa4b5b35a92458aec1b2ce6a2ccffd995a63
earlgray <earlgray283@gmail.com>
2023-03-21 22:13:23 +0900

    style: format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,31 +8,32 @@ module.exports = {
         current: 'currentColor',
         'dark-iceblue': '#5382a1',
         iceblue: '#99b7dc',
-        white: '#ffffffd0',
+        darkwhite: '#ffffffd0'
       },
       fontSize: {
         xs: ['0.85rem'],
+        lg: ['1.0rem']
       },
       animation: {
         // https://tail-animista.vercel.app/play/text/focus-in/text-focus-in
         'text-focus-in':
           'text-focus-in cubic-bezier(0.550, 0.085, 0.680, 0.530) both',
         'text-focus-in-500': 'text-focus-in 500ms',
-        'text-focus-in-1000': 'text-focus-in 1000ms',
+        'text-focus-in-1000': 'text-focus-in 1000ms'
       },
       keyframes: {
         'text-focus-in': {
           '0%': {
             filter: 'blur(12px)',
-            opacity: '0',
+            opacity: '0'
           },
           to: {
             filter: 'blur(0)',
-            opacity: '1',
-          },
-        },
-      },
-    },
+            opacity: '1'
+          }
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
